884e3b562fc529ad3a2a1518ad5848aa20357fb3
Iavor Diatchki <iavor.diatchki@gmail.com>
2024-06-15 01:02:14 -0700

    More operations

--- include/small_bits.h
+++ include/small_bits.h
@@ -6,7 +6,7 @@
 #include <stdint.h>
 #include <assert.h>
 
-// For bits sizes 1 .. 64
+// Global invariant: widths 1 .. 64
 
 
 static inline
@@ -17,6 +17,9 @@ uint64_t small_normalize(size_t w, uint64_t x) {
   uint64_t mask = UINT64_C(-1) << small_padding(w);
   return x & mask;
 }
+
+
+/* Conversions */
 
 static inline
 uint64_t small_to_u64(size_t w, uint64_t x) { return x >> small_padding(w); }
@@ -29,7 +32,6 @@ int64_t u2i_64(uint64_t x) {
   assert(false);
 }
 
-
 static inline
 int64_t small_to_i64(size_t w, uint64_t x) {
   int64_t sx = u2i_64(x);
@@ -40,6 +42,9 @@ static inline
 uint64_t small_from64(size_t w, uint64_t x) {
   return x << small_padding(w);
 }
+
+
+/* Arithmetic */
 
 static inline
 uint64_t small_add(size_t _w, uint64_t x, uint64_t y) { return x + y; }
@@ -65,6 +70,28 @@ uint64_t small_mod(size_t w, uint64_t x, uint64_t y) {
   return small_from64(w, small_to_u64(w,x) % small_to_u64(w,y));
 }
 
+// Signed division.  Throws exception if y is 0. 
+// If y is (-1) may wrap around.
+static inline
+uint64_t small_sdiv(size_t w, uint64_t x, uint64_t y) {
+  int64_t b = small_to_i64(w,y);
+  if (b == INT64_C(-1)) return small_neg(w,x);
+  int64_t a = small_to_i64(w,x);
+  return small_from64(w,a / b);
+}
+
+// Signed modulus. Throws exception if y is 0.
+static inline
+uint64_t small_smod(size_t w, uint64_t x, uint64_t y) {
+  int64_t b = small_to_i64(w,y);
+  if (b == INT64_C(-1)) return 0;
+  int64_t a = small_to_i64(w,x);
+  return small_from64(w, a % b);
+}
+
+
+
+/* Bitwise operations */
 
 static inline
 uint64_t small_and(size_t _w, uint64_t x, uint64_t y) { return x & y; }
@@ -76,13 +103,14 @@ static inline
 uint64_t small_xor(size_t _w, uint64_t x, uint64_t y) { return x ^ y; }
 
 
+
+/* Comparisons */
+
 static inline
 bool small_eq(size_t _w, uint64_t x, uint64_t y) { return x == y; }
 
 static inline
 bool small_neq(size_t _w, uint64_t x, uint64_t y) { return x != y; }
-
-
 
 static inline
 bool small_lt(size_t _w, uint64_t x, uint64_t y) { return x < y; }
@@ -100,6 +128,8 @@ bool small_sleq(size_t _w, uint64_t x, uint64_t y) {
   return u2i_64(x) <= u2i_64(y);
 }
 
+
+/* Shift and Rotates */
 
 static inline
 uint64_t small_shift_left(size_t w, uint64_t x, size_t y) {
@@ -122,6 +152,48 @@ uint64_t small_sshift_right(size_t w, uint64_t x, size_t y) {
   return small_normalize(w,sx >> y);
 }
 
+static inline
+uint64_t small_rotate_left(size_t w, uint64_t x, size_t y) {
+  y %= w;
+  return small_normalize(w, (x << y) | (x >> (w - y)));
+}
+
+static inline
+uint64_t small_rotate_right(size_t w, uint64_t x, size_t y) {
+  y %= w;
+  return small_normalize(w, (x << (w - y)) | (x >> y));
+}
+
+
+
+
+
+/* Splitting and Joining */
+
+// n <= w
+static inline
+uint64_t small_take(size_t w, size_t n, uint64_t x) {
+  return small_normalize(n, x);
+}
+
+// n <= w
+static inline
+uint64_t small_drop(size_t w, size_t n, uint64_t x) {
+  return x << n;
+}
+
+// w1 + w2 <= 64
+static inline
+uint64_t small_append(size_t w1, size_t w2, uint64_t x, uint64_t y) {
+  return x | (y >> w1);
+}
+
+
+
+
+
+/* Iterators */
+// XXX
 
 
 #endif
--- test/main.c
+++ test/main.c
@@ -3,8 +3,8 @@
 
 int main(int argc, char* argv[]) {
   size_t w = 5;
-  uint64_t a = small_from64(w,UINT64_C(-16));
-  uint64_t b = small_from64(w,UINT64_C(-1));
-  printf("%ld\n", small_to_i64(w,small_sdiv(w,a,b)));
+  size_t r = 19;
+  size_t x = small_from64(w,1);
+  printf("%lu -> %lu\n", small_to_u64(w,x), small_to_u64(w,small_rotate_right(w,x,r)));
   return 0;
 }
